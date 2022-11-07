979aabcccc8e446cf87d040808ba437600098de5
Alessia B <borys.alessia@gmail.com>
2022-11-07 17:13:16 +0100

    more moods

--- src/components/MoodPickerDemo.tsx
+++ src/components/MoodPickerDemo.tsx
@@ -1,54 +1,62 @@
 import { useState } from "react";
 
+/* eslint no-console: "error" */
+
 function MoodPickerDemo(): JSX.Element {
   const [moodValueFromCurrentRender, queueRerenderWithNewMoodValue] =
     useState("confused");
 
-  const handleMoodChangeToHappy = () => {
-    const nextMood = "happy";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😀 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
-  };
+  // const handleMoodChangeToHappy = () => {
+  //   queueRerenderWithNewMoodValue("happy");
+  // };
 
   const handleMoodChangeToConfused = () => {
-    const nextMood = "confused";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "🤔 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    queueRerenderWithNewMoodValue("confused");
+    // console.log(
+    //   "🤔 queued a rerender with mood as",
+    //   nextMood,
+    //   "but in this render it is still",
+    //   moodValueFromCurrentRender
+    // );
   };
 
   const handleMoodChangeToSad = () => {
-    const nextMood = "sad";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😢 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    queueRerenderWithNewMoodValue("sad");
+    // console.log(
+    //   "😢 queued a rerender with mood as",
+    //   nextMood,
+    //   "but in this render it is still",
+    //   moodValueFromCurrentRender
+    // );
   };
 
-  console.log(
-    "Component is rendering with a mood value of",
-    moodValueFromCurrentRender
-  );
+  const handleMoodChangeToLaughing = () => {
+    queueRerenderWithNewMoodValue("laughing")
+  };
+  
+  const handleMoodChangeToInLove = () => {
+    queueRerenderWithNewMoodValue("in love")
+  };
+
+  const handleMoodChangeToTired = () => {
+    queueRerenderWithNewMoodValue("tired")
+  };
+  
+  // console.log(
+  //   "Component is rendering with a mood value of",
+  //   moodValueFromCurrentRender
+  // );
 
   return (
     <>
       <h1>Mood Picker Demo</h1>
       <p>Chosen mood: {moodValueFromCurrentRender.toUpperCase()}</p>
-      <button onClick={handleMoodChangeToHappy}>😀</button>
+      <button onClick={() => queueRerenderWithNewMoodValue("happy")}>😀</button>
       <button onClick={handleMoodChangeToConfused}>🤔</button>
       <button onClick={handleMoodChangeToSad}>😢</button>
+      <button onClick={handleMoodChangeToLaughing}>😂</button>
+      <button onClick={handleMoodChangeToInLove}>😘</button>
+      <button onClick={handleMoodChangeToTired}>😪</button>
     </>
   );
 }
